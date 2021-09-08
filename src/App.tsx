0baf2a17e31e31179161a7daff237066910f0372
Valerie <valerieloveland@gmail.com>
2021-09-08 08:52:20 -0400

    Took the date function out of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
+import { Date } from "./Date";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Event</p>
-        <p>Date Time</p>
+        <Date />
         <p>Years Months Weeks Days Hours</p>
         <p>Slider</p>
       </header>
